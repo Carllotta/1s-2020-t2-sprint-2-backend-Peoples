445ec9fd4fdf9a5df58724a785615095e25d516f
Carllotta <carllottadias@gmail.com>
2020-02-20 11:26:38 -0300

    Adicionando a primeira parte do projeto

---
+++ Scripts/SQLQuery-01-DDL-PEOPLES.sql
@@ -0,0 +1,12 @@
+-- DDL 
+
+
+ CREATE DATABASE M_Peoples;
+
+ USE M_Peoples;
+
+ CREATE TABLE Funcionarios(
+ID_Funcionario	INT IDENTITY PRIMARY KEY,
+Nome			VARCHAR(200) NOT NULL,
+Sobrenome		VARCHAR(200) NOT NULL
+);
